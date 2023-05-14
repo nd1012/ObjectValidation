0e8ec8b83b4e7266e95ea35a9a4d1a354bffe751
nd <nd1012@users.noreply.github.com>
2023-05-14 22:16:02 +0200

    Update

--- src/ObjectValidation/NoValidationAttribute.cs
+++ src/ObjectValidation/NoValidationAttribute.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+//TODO Optional skip NULL value check
+
 namespace wan24.ObjectValidation
 {
     /// <summary>
-    /// Attribute for types or properties which shouldn't be validated
+    /// Attribute for types or properties which shouldn't be validated (optional completely)
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
     public sealed class NoValidationAttribute : ValidationAttribute
@@ -12,6 +14,11 @@ namespace wan24.ObjectValidation
         /// Constructor
         /// </summary>
         public NoValidationAttribute() : base() { }
+        
+        /// <summary>
+        /// Skip the <see langword="null"/> value check(s)?
+        /// </summary>
+        public bool SkipNullValueCheck { get; set; }
 
         /// <inheritdoc/>
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) => null;
--- src/ObjectValidation/ValidatableObject.cs
+++ src/ObjectValidation/ValidatableObject.cs
@@ -3,8 +3,8 @@
     /// <summary>
     /// Base class for a validatable object
     /// </summary>
-    [Obsolete("Use ValidatableObjectBase instead")]
-    public abstract class ValidatableObject : ValidatableObjectBase
+    [Obsolete("Use ValidatableObjectBase instead (ValidatableObject will be removed in v2!)")]
+    public abstract class ValidatableObject : ValidatableObjectBase //TODO Remove in v2
     {
         /// <summary>
         /// Constructor
