7b16796103a14358c84aa590e6fbb3423e85b853
nd <nd1012@users.noreply.github.com>
2024-02-15 07:20:50 +0100

    Update
    + Added runtime property count limit to `CountLimitAttribute`

--- src/ObjectValidation/CountLimitAttribute.cs
+++ src/ObjectValidation/CountLimitAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace wan24.ObjectValidation
 {
@@ -11,10 +11,27 @@ namespace wan24.ObjectValidation
     public class CountLimitAttribute : ValidationAttribute
     {
         /// <summary>
+        /// Minimum
+        /// </summary>
+        protected readonly long? _Min = null;
+        /// <summary>
+        /// Maximum
+        /// </summary>
+        protected readonly long _Max = 0;
+        /// <summary>
+        /// Minimum getter
+        /// </summary>
+        protected readonly PropertyInfo? _MinGetter = null;
+        /// <summary>
+        /// Maximum getter
+        /// </summary>
+        protected readonly PropertyInfo? _MaxGetter = null;
+
+        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="max">Maximum</param>
-        public CountLimitAttribute(long max) : base() => Max = max;
+        public CountLimitAttribute(long max) : base() => _Max = max;
 
         /// <summary>
         /// Constructor
@@ -23,19 +40,69 @@ namespace wan24.ObjectValidation
         /// <param name="max">Maximum</param>
         public CountLimitAttribute(long min, long max) : base()
         {
-            Min = min;
-            Max = max;
+            _Min = min;
+            _Max = max;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="maxGetter">Maximum getter static property</param>
+        /// <param name="minGetter">Minimum getter static property</param>
+        /// <param name="min">Minimum</param>
+        public CountLimitAttribute(string maxGetter, string? minGetter = null, long? min = null) : base()
+        {
+            _Min = min;
+            MinGetter = minGetter;
+            MaxGetter = maxGetter;
+            string[] info;
+            if (minGetter is not null)
+            {
+                info = minGetter.Split('.');
+                if (info.Length == 1) throw new ArgumentException("Invalid min. getter", nameof(minGetter));
+                Type type = Type.GetType(string.Join('.', info.Take(info.Length - 1)))
+                    ?? throw new ArgumentException($"Can't load type \"{string.Join('.', info.Take(info.Length - 1))}\"", nameof(minGetter));
+                _MinGetter = type.GetProperty(info[^1], BindingFlags.Public | BindingFlags.Static)
+                    ?? throw new ArgumentException("Can't load public static property", nameof(minGetter));
+            }
+            {
+                info = maxGetter.Split('.');
+                if (info.Length == 1) throw new ArgumentException("Invalid max. getter", nameof(maxGetter));
+                Type type = Type.GetType(string.Join('.', info.Take(info.Length - 1)))
+                    ?? throw new ArgumentException($"Can't load type \"{string.Join('.', info.Take(info.Length - 1))}\"", nameof(maxGetter));
+                _MaxGetter = type.GetProperty(info[^1], BindingFlags.Public | BindingFlags.Static)
+                    ?? throw new ArgumentException("Can't load public static property", nameof(maxGetter));
+            }
         }
 
         /// <summary>
         /// Minimum
         /// </summary>
-        public long? Min { get; }
+        public long? Min
+        {
+            get
+            {
+                if (_MinGetter is null) return _Min;
+                object? value = _MinGetter.GetValue(obj: null);
+                if (value is null) return null;
+                return (long)Convert.ChangeType(value, typeof(long));
+            }
+        }
 
         /// <summary>
         /// Maximum
         /// </summary>
-        public long Max { get; }
+        public long Max => _MaxGetter is null ? _Max : (long)Convert.ChangeType(_MaxGetter.GetValue(obj: null) ?? throw new InvalidProgramException("Maximum getter returned NULL"), typeof(long));
+
+        /// <summary>
+        /// Minimum getter static property
+        /// </summary>
+        public string? MinGetter { get; }
+
+        /// <summary>
+        /// Maximum getter static property
+        /// </summary>
+        public string? MaxGetter { get; }
 
         /// <summary>
         /// Get the error message
